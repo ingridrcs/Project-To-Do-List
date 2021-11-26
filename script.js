68191d715b540cb5954e3d1f3f51cf482f838d6e
Ingrid_Ramos <ingridrcs@gmail.com>
2021-11-26 13:14:44 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -4,45 +4,54 @@ let onbutton = document.getElementById('criar-tarefa');
 // Consegui fazer o exercício 5 e 6 e 7 em função das mentorias durante o dia do projeto 
 onbutton.addEventListener('click', function () {
 
-  let keepLi = tarefa.value;
+  const keepLi = tarefa.value;
   console.log(keepLi);
-  let newItem = document.createElement('li');
+  const newItem = document.createElement('li');
 
   newItem.innerText = keepLi;
   console.log(newItem);
-  let list = document.getElementById('lista-tarefas');
+  const list = document.getElementById('lista-tarefas');
   list.appendChild(newItem);
   tarefa.value = '';
+  newItem.className = 'selected';
+
 
   newItem.addEventListener('click', function () {
     newItem.style.backgroundColor = 'rgb(128, 128, 128)';
-    newItem.className = 'selected';
-    
-    //Tentativas do requisito 8
-    //newItem.addEventListener('click', function () {
-    //newItem.ad
-    //newItem.style.backgroundColor = 'rgb(128, 128, 128)';
-    //newItem.className='selected';
+    newItem.addEventListener('dblclick', risca);
+    // Tentativas do requisito 8
+    // newItem.addEventListener('click', function () {
+    // newItem.ad
+    // newItem.style.backgroundColor = 'rgb(128, 128, 128)';
+    // newItem.className='selected';
+
+    // for(let index of listTotal) {
+    // index.addEventListener('click', function(event) {
+    // listLi.classList.remove('listLi')
+    // event.target.classList.add('listLi');
+    // offColor();
   })
 
+  /* REQUISITO-9 Source: A opção do uso do Toggle eu vi através do slack e as monitorias :https://trybecourse.slack.com/archives/C02HY11SPJP/p1637708722293500
+  https://www.w3schools.com/jsref/event_ondblclick.asp
+  https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/toggle*/
+  function risca(event) {
+    event.target.classList.toggle('completed');
+  }
+});
 
-  //for(let index of listTotal) {
-  //index.addEventListener('click', function(event) {
-  //listLi.classList.remove('listLi')
-  //event.target.classList.add('listLi');
-  //offColor();
-})
-//REQUISITO-10 - Tive muitas dúvidas em remover as 'li' da 'ol'. Consegui resolver através das monitorias e pesquisas na internet
-//Source: https://www.youtube.com/watch?v=pSkaIyRAt7w
+// REQUISITO-10 - Tive muitas dúvidas em remover as 'li' da 'ol'. Consegui resolver através das monitorias e pesquisas na internet
+// Source: https://www.youtube.com/watch?v=pSkaIyRAt7w
+// https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
 let botaoApagaTudo = document.querySelector('#apaga-tudo');
 
 botaoApagaTudo.addEventListener('click', apaga)
 function apaga() {
-let pai= document.querySelector('#lista-tarefas');
-pai.parentNode.removeChild(pai);
-  }
+  let pai = document.querySelector('#lista-tarefas');
+  pai.parentNode.removeChild(pai);
+}
 
-//Outra opção para Requisito 7
+// Outra opção para Requisito 7
 /*function offColor() {
 
   let listTotal = document.getElementById('lista-tarefas').children;
